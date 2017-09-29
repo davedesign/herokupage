a42f15c464a28b01069cb9c1463be3015faba407
davedesign <32309481+davedesign@users.noreply.github.com>
2017-09-29 13:20:14 -0500

    Carga de archivos

---
+++ formulario.php
@@ -0,0 +1,39 @@
+﻿<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Felicidades</title>
+    </head>
+
+    <body>
+
+      <?php
+        
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+        $correo = $_POST["usremail"];
+        $telefono = $_POST["usrtel"];
+        $mensaje = $_POST["msje"];
+
+        $para = "davidcastillodg@gmail.com";
+
+        $asunto = "Mensaje Nuevo";
+
+        $contenido = "
+        
+        Nombre del remitente: ".$nombre."
+        Apellido: ".$apellido."
+        E-mail: ".$correo."
+        Telefono: ".$telefono."
+        Mensaje: ".$mensaje."
+        ";
+
+        mail($para,$asunto,utf8_decode($contenido));
+
+        echo "<h1>Se envio correctamente</h1>";
+
+      ?>
+
+    </body>
+
+  </html>
