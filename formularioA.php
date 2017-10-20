2b64f9a5ee051a72917848d2a5bd9ceac4c35298
davedesign <32309481+davedesign@users.noreply.github.com>
2017-10-19 20:09:43 -0500

    Add files via upload
    Nuevo archivo php formulario

---
+++ formularioA.php
@@ -0,0 +1,26 @@
+
+      <?php
+
+        require 'phpmailer/PHPMailer.php';
+
+        $mail = new PHPMailer();
+
+        $mail->isSMTP();
+        $mail->Host ='smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'davidcastillodg@gmail.com';
+        $mail->Password = '83122612';
+
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom('davidcastillodg@gmail.com');
+        $mail->addAddress('davidcastillodg@gmail.com');
+
+        $mail->Subject = 'Mensaje Nuevo';
+        $mail->Body = 'Este es el cuerpo del correo';
+
+        $mail->send();
+
+
+      ?>
